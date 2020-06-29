d96ae9d540d7e1f09ff76dbcb3b91d98dff8dea5
ROU LEE <leerou59@gmail.com>
2020-06-29 21:54:22 +0800

    First Commit

---
+++ app/src/main/java/com/swe401/getasset/MainActivity.java
@@ -0,0 +1,14 @@
+package com.swe401.getasset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
